5fb972081defdc9b0a38a1601a2a1acad19a8107
Simphiwe Madi <smadi021@student.wethinkcode.co.za>
2023-03-08 14:38:24 +0200

    Freedom of the press task

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1208/Solution.java
@@ -0,0 +1,37 @@
+package com.codegym.task.task12.task1208;
+
+/* 
+Freedom of the press
+
+*/
+
+import java.util.Date;
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+    public static void print(int number) {
+    
+    }
+    
+    public static void print(String text) {
+    
+    }
+    
+    public static void print(String text, int number) {
+    
+    }
+    
+    public static void print(long idNumber) {
+    
+    }
+    
+    public static void print(Date dob) {
+    
+    }
+    
+    
+}
