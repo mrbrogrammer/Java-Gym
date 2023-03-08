5bcb783a501b24a47d5b0c920c18f58890b89154
Simphiwe Madi <smadi021@student.wethinkcode.co.za>
2023-03-08 14:33:27 +0200

    int and Integer task

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1207/Solution.java
@@ -0,0 +1,23 @@
+package com.codegym.task.task12.task1207;
+
+/* 
+int and Integer
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        print(2);
+        print(new Integer(3));
+
+    }
+
+    //write your code here
+    public static void print(int number) {
+    
+    }
+    
+    public static void print(Integer number) {
+    
+    }
+}
