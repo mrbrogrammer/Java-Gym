7f8256e21c05050aeee2c699c4846021f37c8db0
Simphiwe Madi <smadi021@student.wethinkcode.co.za>
2023-03-08 14:29:36 +0200

    We're on overload! task

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1206/Solution.java
@@ -0,0 +1,21 @@
+package com.codegym.task.task12.task1206;
+
+/* 
+We're on overload!
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+    public static void print(String string) {
+    
+    }
+    
+    public static void print(int integer) {
+    
+    }
+}
