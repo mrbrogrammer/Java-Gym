81b75d024b67405dbc7376285c2b5e75c872ea7c
Simphiwe Madi <smadi021@student.wethinkcode.co.za>
2023-03-08 14:46:39 +0200

    Three methods and a maximum task

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1210/Solution.java
@@ -0,0 +1,26 @@
+package com.codegym.task.task12.task1210;
+
+/* 
+Three methods and a maximum
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+    
+    public static int max(int a, int b) {
+        return Math.max(a, b);
+    }
+    
+    public static long max(long a, long b) {
+        return Math.max(a, b);
+    }
+    
+    public static double max(double a, double b) {
+        return Math.max(a, b);
+    }
+}
