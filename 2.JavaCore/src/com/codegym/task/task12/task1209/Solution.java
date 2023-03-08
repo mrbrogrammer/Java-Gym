2f13fcbe7f29570c55d9ded09da4c9badf8d0e19
Simphiwe Madi <smadi021@student.wethinkcode.co.za>
2023-03-08 14:43:04 +0200

    Three methods and a minimum task

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1209/Solution.java
@@ -0,0 +1,25 @@
+package com.codegym.task.task12.task1209;
+
+/* 
+Three methods and a minimum
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+
+    }
+
+    //write your code here
+    public static int min(int a, int b) {
+        return Math.min(a, b);
+    }
+    
+    public static long min(long a, long b) {
+        return Math.min(a, b);
+    }
+    
+    public static double min(double a, double b) {
+        return Math.min(a, b);
+    }
+}
