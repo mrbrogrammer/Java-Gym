3168d3ec89123b8dba82f48629e92c97c0e375f9
Simphiwe Madi <smadi021@student.wethinkcode.co.za>
2023-03-09 11:41:18 +0200

    Parent of the CTO class

---
+++ 2.JavaCore/src/com/codegym/task/task12/task1229/Solution.java
@@ -0,0 +1,30 @@
+package com.codegym.task.task12.task1229;
+
+/* 
+Parent of the CTO class
+
+*/
+
+public class Solution {
+
+    public static void main(String[] args) {
+        CTO cto = new CTO();
+        System.out.println(cto);
+    }
+
+    public static interface Businessman {
+        public void workHard();
+    }
+    
+    public static class Employee implements Businessman {
+    
+        @Override
+        public void workHard() {
+        
+        }
+    }
+
+    public static class CTO extends Employee implements Businessman {
+
+    }
+}
