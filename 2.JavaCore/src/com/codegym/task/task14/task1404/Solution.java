6726796856d9c14e94b58a7cd727dcf008428457
Simphiwe Madi <smadi021@student.wethinkcode.co.za>
2023-03-13 11:05:18 +0200

    Building and School

---
+++ 2.JavaCore/src/com/codegym/task/task14/task1404/Solution.java
@@ -0,0 +1,69 @@
+package com.codegym.task.task14.task1404;
+
+/* 
+Cats
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //write your code here
+    }
+
+    static class CatFactory {
+        static Cat getCatByKey(String key) {
+            Cat cat;
+            switch (key) {
+                case "feral":
+                    cat = new MeanCat("Claws");
+                    break;
+                case "miss":
+                    cat = new NiceCat("Missy");
+                    break;
+                case "smudge":
+                    cat = new NiceCat("Smudgey");
+                    break;
+                default:
+                    cat = new Cat(key);
+                    break;
+            }
+            return cat;
+        }
+    }
+
+    static class Cat {
+        private String name;
+
+        protected Cat(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return this.name;
+        }
+
+        public String toString() {
+            return "I'm " + getName() + ", an alley cat";
+        }
+    }
+
+    static class MeanCat extends Cat {
+        MeanCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "I'm " + getName() + ", and I'm going to claw your eyes out";
+        }
+    }
+
+    static class NiceCat extends Cat {
+        NiceCat(String name) {
+            super(name);
+        }
+
+        public String toString() {
+            return "I'm a nice kitten named " + getName();
+        }
+    }
+}
