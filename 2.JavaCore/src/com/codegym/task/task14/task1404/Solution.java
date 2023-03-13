8e83c7cd6a992546726f16e743adc451a8f5829d
Simphiwe Madi <smadi021@student.wethinkcode.co.za>
2023-03-13 11:43:07 +0200

    Cats

--- 2.JavaCore/src/com/codegym/task/task14/task1404/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task14/task1404/Solution.java
@@ -5,9 +5,36 @@ Cats
 
 */
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
 public class Solution {
     public static void main(String[] args) throws Exception {
         //write your code here
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        
+        String string;
+    
+        ArrayList<String> stringsParameters = new ArrayList();
+        do {
+            string = bufferedReader.readLine();
+            if (string.equals("")) continue;
+            stringsParameters.add(string);
+        } while (!(string.isEmpty()));
+        
+        
+        ArrayList<Cat> cats = new ArrayList<>();
+        
+        stringsParameters.forEach(x -> cats.add(CatFactory.getCatByKey(x)));
+    
+        Iterator<Cat> iterator = cats.iterator();
+        
+        while (iterator.hasNext()){
+            System.out.println(iterator.next().toString());
+        }
     }
 
     static class CatFactory {
