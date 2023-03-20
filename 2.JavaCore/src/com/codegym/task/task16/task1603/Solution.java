cf611b75ba78e6caaba6256ca523156be17f136a
Simphiwe Madi <smadi021@student.wethinkcode.co.za>
2023-03-20 09:45:38 +0200

    A list and some threads

---
+++ 2.JavaCore/src/com/codegym/task/task16/task1603/Solution.java
@@ -0,0 +1,29 @@
+package com.codegym.task.task16.task1603;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+A list and some threads
+
+*/
+
+public class Solution {
+    public static volatile List<Thread> list = new ArrayList<>(5);
+
+    public static void main(String[] args) {
+        
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        
+    }
+
+    public static class SpecialThread implements Runnable {
+        public void run() {
+            System.out.println("This is the run method inside SpecialThread");
+        }
+    }
+}
