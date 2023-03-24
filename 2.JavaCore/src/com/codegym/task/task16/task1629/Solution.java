f9c18562001e029f0593b985bd7b6d325ad377ae
Simphiwe Madi <smadi021@student.wethinkcode.co.za>
2023-03-24 11:46:31 +0200

    Notes

--- 2.JavaCore/src/com/codegym/task/task16/task1629/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task16/task1629/Solution.java
@@ -1,54 +1,50 @@
-//package com.codegym.task.task16.task1629;
-//
-//import java.io.BufferedReader;
-//import java.io.IOException;
-//import java.io.InputStreamReader;
-//
-///**
-// * Take your turn!
-// * 1. In the Solution class, create a public static Read3Strings class that inherits Thread.
-// * 2. In the run method, read three lines from the console.
-// * 3. Three lines should be read in succession on one thread and combined into one space-delimited string.
-// * 4. In the main method, display the result for each thread.
-// * 5. Use join.
-// *
-// * Example:
-// *
-// * Input:
-// * a
-// * b
-// * c
-// * d
-// * e
-// * f
-// *
-// * Output:
-// * a b c
-// * d e f
-// *
-// *
-// * Requirements:
-// * 1. In the Solution class, declare the public static Read3Strings class.
-// * 2. The Read3Strings class should inherit the Thread class.
-// * 3. The Read3Strings class's run method must read three lines.
-// * 4. The Read3Strings class must have a public printResult method.
-// * 5. The printResult method should display the 3 read lines, separating them with spaces.
-// * 6. The main method should call the created threads' start methods.
-// * 7. The main method should call the created threads' join methods.
-// * 8. The program's output should indicate that the threads read the lines sequentially, not in parallel.
-// */
-//public class Solution {
-//    public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//
-//    public static void main(String[] args) throws InterruptedException {
-//        Read3Strings t1 = new Read3Strings();
-//        Read3Strings t2 = new Read3Strings();
-//
-//        //write your code here
-//
-//        t1.printResult();
-//        t2.printResult();
-//    }
-//
-//    //write your code here
-//}
+package com.codegym.task.task16.task1629;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Take your turn!
+ */
+public class Solution {
+    public static volatile BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws InterruptedException {
+        Read3Strings t1 = new Read3Strings();
+        Read3Strings t2 = new Read3Strings();
+
+        //write your code here
+        t1.start();
+        t1.join();
+        t2.start();
+        t2.join();
+        
+        t1.printResult();
+        t2.printResult();
+    }
+
+    //write your code here
+    public static class Read3Strings extends Thread{
+        List<String> letters = new ArrayList<>();
+        @Override
+        public void run() {
+            for (int index = 0; index < 3; index++) {
+                try {
+                   letters.add(reader.readLine());
+                } catch (IOException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        }
+    
+        public void printResult() {
+            for (int index = 0; index < letters.size(); index++) {
+                System.out.print(letters.get(index) + " ");
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ 2.JavaCore/src/com/codegym/task/task17/task1701/Solution.java
@@ -0,0 +1,52 @@
+package com.codegym.task.task17.task1701;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Notes
+
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        new NoteThread().start();
+        new NoteThread().start();
+    }
+
+    public static class Note {
+
+        public static final List<String> notes = new ArrayList<>();
+
+        public static void addNote(String note) {
+            notes.add(0, note);
+        }
+
+        public static void removeNote(String threadName) {
+            String note = notes.remove(0);
+            if (note == null) {
+                System.out.println("Another thread deleted our note");
+            } else if (!note.startsWith(threadName)) {
+                System.out.println("Thread [" + threadName + "] deleted someone else's note [" + note + "]");
+            } else {
+                System.out.println("Thread [" + threadName + "] deleted its own note [" + note + "]");
+            }
+        }
+    }
+    
+    public static class NoteThread extends Thread {
+    
+        @Override
+        public void run() {
+            for (int index = 0; index < 1000; index++) {
+                Solution.Note.addNote(this.getName() + "-Note" + index);
+                try {
+                    Thread.sleep(1);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+                Solution.Note.removeNote(this.getName());
+            }
+        }
+    }
+}
