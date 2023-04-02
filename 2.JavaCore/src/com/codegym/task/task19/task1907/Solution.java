6a10e5bf5e4bb3d96479aab48ae782c56af2dff5
mrbrogrammer <seesimbarun@gmail.com>
2023-04-02 22:46:11 +0200

    Counting words

--- 2.JavaCore/src/com/codegym/task/task19/task1907/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task19/task1907/Solution.java
@@ -6,13 +6,46 @@ Counting words
 */
 
 import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
 
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = null;
+        FileReader reader = null;
     
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        try {
+            bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+            reader = new FileReader(bufferedReader.readLine());
+            
+            
+            StringBuilder text = new StringBuilder();
+            while (reader.ready()) {
+                char c = (char) reader.read();
+                text.append(c);
+                
+            }
+         
+            String[] result = text.toString().split("\\W");
+    
+            int count = 0;
+            for (int index = 0; index < result.length; index++) {
+               if (Objects.equals(result[index], "world")){
+                   count++;
+               }
+            }
+
+            System.out.println(count);
+    
+        } finally {
+            assert reader != null;
+            reader.close();
+            bufferedReader.close();
         
-        
+        }
     }
 }
