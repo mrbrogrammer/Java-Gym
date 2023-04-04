62c4afdc4232d43d3fb0778073f6c5fed202d1c1
mrbrogrammer <seesimbarun@gmail.com>
2023-04-05 00:47:53 +0200

    Mirror image

--- 2.JavaCore/src/com/codegym/task/task19/task1926/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task19/task1926/Solution.java
@@ -5,7 +5,32 @@ Mirror image
 
 */
 
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Solution {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader inStream = new BufferedReader(new FileReader(reader.readLine()));
+        reader.close();
+    
+        List<String> lines = new ArrayList<>();
+        while (inStream.ready()) {
+            lines.add(inStream.readLine());
+        }
+        inStream.close();
+    
+        for (int index = 0; index < lines.size(); index++) {
+            String string = lines.get(index);
+            for (int i = string.length() - 1; i >= 0; i--) {
+                System.out.print(string.charAt(i));
+            }
+            System.out.println();
+            
+        }
     }
 }
