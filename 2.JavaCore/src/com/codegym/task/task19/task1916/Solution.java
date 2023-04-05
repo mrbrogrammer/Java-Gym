90340e9d2761551a27aa1b4ecda9c8ca10158305
mrbrogrammer <seesimbarun@gmail.com>
2023-04-05 09:14:42 +0200

    Searching for the right lines2

--- 2.JavaCore/src/com/codegym/task/task19/task1916/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task19/task1916/Solution.java
@@ -1,9 +1,7 @@
 package com.codegym.task.task19.task1916;
 
 import java.io.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 /* 
 Tracking changes
@@ -11,132 +9,134 @@ Tracking changes
 */
 
 public class Solution {
+    static boolean debug = false; // debug mode true = on; false = off
+    
     public static List<LineItem> lines = new ArrayList<>();
     
-    @Override
-    public boolean equals(Object obj) {
-        return super.equals(obj);
-    }
-    
     public static void main(String[] args) throws IOException {
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-    
-        BufferedReader file1InStream = new BufferedReader(new FileReader("/Users/admin/Downloads/CodeGymTasks/2.JavaCore/src/com/codegym/task/task19/task1916/file1.txt"));
-        BufferedReader file2InStream = new BufferedReader(new FileReader("/Users/admin/Downloads/CodeGymTasks/2.JavaCore/src/com/codegym/task/task19/task1916/file2.txt"));
-    
-        List<String> file1 = new ArrayList<>();
-        while (file1InStream.ready()) {
-            String line = file1InStream.readLine();
-            file1.add(line);
-        }
+        String[] fileNames = new String[2];
+        BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+        fileNames[0] = userInput.readLine();
+        fileNames[1] = userInput.readLine();
+        userInput.close();
         
-        List<String> file2 = new ArrayList<>();
-        while (file2InStream.ready()) {
-            String line = file2InStream.readLine();
-            file2.add(line);
-        }
         
-        int size = file1.size();
-        if (file1.size() < file2.size()) size = file2.size();
+        //declare array lists to hold each files contents
+        ArrayList<String> fileOneContents = getContents(fileNames[0]), fileTwoContents = getContents(fileNames[1]);
+        //if(debug) System.out.println(fileOneContents + "\n" + fileTwoContents);
+        
+        //creates LinkedHashMap and delegates sorting to method
+        LinkedHashMap<String,Type> orderedResults = sortFileContents(fileOneContents, fileTwoContents);
     
-        for (int i1 = 0, i2 = 0; i1 < file2.size();) {
-            
-            if (file1.get(i1).hashCode() == file2.get(i2).hashCode()) {
-                LineItem item = new LineItem(Type.SAME, Type.SAME + " " + file1.get(i1));
-                lines.add(item);
-                System.out.println("same");
-//                continue;
-            } else {
+        if (debug) for(Map.Entry<String, Type> pair : orderedResults.entrySet()) System.out.println(pair.getValue() + " : " + pair.getKey());
+        
+        //adds each item from linkedhashmap to static list: lines
+        for(Map.Entry<String,Type> pair : orderedResults.entrySet()){
+            lines.add(new LineItem(pair.getValue(), pair.getKey()));
+        }
     
-//                int t = Arrays.binarySearch(file2.toArray(), file1.get(i1));
-//
-//                System.out.println(t);
-    
-                int countRemove = 0;
-                int countAdded = 0;
-                for (int j = 0; j < file2.size(); j++) {
-                    if (file1.get(i1).hashCode() == file2.get(j).hashCode()) {
-                        countRemove++;
-                    }
-                }
-    
-                if (countRemove > 0) {
-                } else {
-                    LineItem item = new LineItem(Type.REMOVED, Type.REMOVED + " " + file1.get(i1));
-                    System.out.println("removed");
-                    lines.add(item);
-                }
-                for (int k = 0; k < file1.size(); k++) {
-                    if (file2.get(i2).hashCode() == file1.get(k).hashCode()) {
-                        countAdded++;
-                    }
-                }
-                
-                if (countAdded > 0) {
-                
-                } else {
-                    LineItem item = new LineItem(Type.ADDED, Type.ADDED + " " + file2.get(i1));
-                    System.out.println("added");
-                    lines.add(item);
-                    i1--;
-                }
-                
+        if(debug) {
+            for (int i = 0; i < lines.size(); i++) {
+                System.out.println(lines.get(i).type + " " + lines.get(i).line);
             }
-            i1++;
-            i2++;
         }
-        
-        
-        
-        
-//        for (int index = 0; index < file1.size(); index++) {
-//            try {
-//                if (file1.get(index).hashCode() == file2.get(index).hashCode()) {
-//                    LineItem item = new LineItem(Type.SAME, Type.SAME + " " + file1.get(index));
-//                    lines.add(item);
-//                } else {
-//                    LineItem item = new LineItem(Type.REMOVED, Type.REMOVED + " " + file1.get(index));
-//                    lines.add(item);
-//                }
-//            } catch (IndexOutOfBoundsException e) {
-//                LineItem item = new LineItem(Type.REMOVED, Type.REMOVED + " " +  file1.get(index));
-//                lines.add(item);
-//            }
-//        }
-//
-//        for (int index = 0; index < file2.size(); index++) {
-//            try {
-//                if (file2.get(index).hashCode() != file1.get(index).hashCode()) {
-//                    LineItem item = new LineItem(Type.ADDED, Type.ADDED + " " +  file2.get(index));
-//                    lines.add(item);
-//                }
-//            } catch (IndexOutOfBoundsException e) {
-//                LineItem item = new LineItem(Type.ADDED,Type.ADDED + " " + file2.get(index));
-//                lines.add(item);
-//            }
-//        }
-    
-        for (LineItem pair : lines) {
-            System.out.println(pair.line);
-        }
-    
-        bufferedReader.close();
-        file1InStream.close();
-        file2InStream.close();
-//        System.out.println(lines.get(0).line);
 //        System.out.println(lines.get(0).type);
+//        System.out.println(lines.get(0).line);
     }
-
+        
+        // orders the file content's passed to it and tags each line as same, added, removed; adds them to linkedhashmap which is returned
+        private static LinkedHashMap<String, Type> sortFileContents(ArrayList<String> fileOneContents, ArrayList<String> fileTwoContents) {
+            LinkedHashMap<String, Type> returnValues = new LinkedHashMap<>();
+            ArrayList<String> order;
+            
+            // initializes first values of the order: adds SAME and REMOVED
+            order = getStartingOrder(fileOneContents, fileTwoContents);
+            
+            // enter all lines to be added in correct order
+            addInsterts(order, fileOneContents, fileTwoContents);
+            
+            // enters everything into the linked hash map
+            for(String x : order){
+                Type type = null;
+                String[] values = x.split("亦");
+                
+                switch (values[0]){
+                    case "ADDED":
+                        type = Type.ADDED;
+                        break;
+                    case "REMOVED":
+                        type = Type.REMOVED;
+                        break;
+                    case "SAME":
+                        type = Type.SAME;
+                        break;
+                    default:
+                        break;
+                }
+                returnValues.put(values[1],type);
+            }
+            
+            return returnValues;
+        }
+        
+        // adds in all new lines of code at the index 1 after the last SAME, accounts for REMOVE
+        private static void addInsterts(ArrayList<String> order, ArrayList<String> StartingFile, ArrayList<String> updatedFile) {
+            int index = 0;
+            
+            //iterates through each line of the updated file
+            for(int i = 0; i < updatedFile.size();i++){
+                
+                // step one, check if the line is contained, increases index number if true
+                if(StartingFile.contains(updatedFile.get(i)))
+                    index++;
+                else{
+                    // step 2 if 1 is false: adds line at element index, increases index, or if index is past size, puts it at the end
+                    if(index != order.size()){
+                        order.add(index, "ADDED"+"亦"+updatedFile.get(i));
+                        index++;
+                    }else{
+                        order.add("ADDED"+"亦"+updatedFile.get(i));
+//                        index++; //keeps it at the end
+                    }
+                }
+                
+                // increases index if a REMOVE is found
+                if(index != order.size() && order.get(index).contains("REMOVED"))
+                    index++;
+            }
+        }
+        
+        //Starts the ordered array with all the lines from the first file, checks if they are the same or need to be removed
+        private static ArrayList<String> getStartingOrder(ArrayList<String> StartingFile, ArrayList<String> updatedFile) {
+            ArrayList<String> order = new ArrayList<>();
+            
+            //foreach string in the first file passed to it, use a chinese character so that i can split type from line later on
+            for (String x: StartingFile){
+                if(updatedFile.contains(x)) order.add("SAME"+"亦"+x);
+                else order.add("REMOVED"+ "亦" + x);
+            }
+            return order;
+        }
+        
+        //reads the file from the file name passed to it, returns a string arraylist of each line
+        private static ArrayList<String> getContents(String fileName) throws IOException {
+            ArrayList<String> returnValues = new ArrayList<>();
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            while(reader.ready()) returnValues.add(reader.readLine());
+            reader.close();
+            return returnValues;
+        }
+    
     public static enum Type {
         ADDED,        // New line added
         REMOVED,      // Line deleted
         SAME          // No change
     }
-
+    
     public static class LineItem {
         public Type type;
         public String line;
-
+        
         public LineItem(Type type, String line) {
             this.type = type;
             this.line = line;
--- 2.JavaCore/src/com/codegym/task/task19/task1922/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task19/task1922/Solution.java
@@ -17,66 +17,35 @@ public class Solution {
     public static List<String> words = new ArrayList<>();
 
     static {
-        words.add("file");
-        words.add("view");
-        words.add("In");
+        words.add("A");
+        words.add("B");
+        words.add("C");
     }
 
     public static void main(String[] args) throws IOException {
         // must add input from console
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
-        
-        BufferedReader inStream = new BufferedReader(new FileReader(bufferedReader.readLine()));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String file = br.readLine();
+        BufferedReader br1 = new BufferedReader(new FileReader(file));
+        br.close();
+        String s ;
     
-        List<String> list = new ArrayList<>();
-        while (inStream.ready()) {
-            String string = inStream.readLine();
-            list.add(string);
-        }
-    
-        inStream.close();
-    
-    
-        StringBuffer word = new StringBuffer();
-        for (int index = 0; index < list.size(); index++) {
-            
-            // the number of words differs. So, split accordingly
-            String[] strings = list.get(index).split(" ");
-            
-            String line = "";
+        while (br1.ready()) {
+            s = br1.readLine();
+            String[] str = s.split(" ");
             int count = 0;
-            int counter = 0;
-            int wrong = 0;
-            int size = Math.max(strings.length, words.size());
-            for (int i = 0; i < size; i++) {
-                // possibly check all elements.
-                try {
-                    for (int j = 0; j < strings.length; j++) {
-                        if (words.get(i).equals(strings[j])) {
-                            count++;
-                        } else {
-                            wrong++;
-                        }
-                        
+            for (String s1 : str) {
+                
+                for (String word : words) {
+                    if (s1.equals(word)) {
+                        count++;
                     }
-                } catch(IndexOutOfBoundsException e) {
-        
                 }
-                
-                line += " " + strings[i];
-                
-               counter++;
-               
             }
-            
-            System.out.println("wrong = " + wrong / words.size());
             if (count == 2) {
-                System.out.println(line.trim());
+                System.out.println(s);
             }
         }
-        
-        
-        bufferedReader.close();
-        inStream.close();
+        br1.close();
     }
 }
