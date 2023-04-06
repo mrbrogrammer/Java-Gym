0287770e1ebe55eab3f2f6302b0c618f23391f42
mrbrogrammer <seesimbarun@gmail.com>
2023-04-06 12:08:49 +0200

    How do you serialize Singleton?

--- 2.JavaCore/src/com/codegym/task/task20/task2002/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task20/task2002/Solution.java
@@ -1,6 +1,7 @@
 package com.codegym.task.task20.task2002;
 
 import java.io.*;
+import java.text.SimpleDateFormat;
 import java.util.*;
 
 /* 
@@ -42,53 +43,72 @@ public class Solution {
 
     public static class CodeGym {
         public List<User> users = new ArrayList<>();
-
+    
         public void save(OutputStream outputStream) throws Exception {
-            // Implement this method
-            
-            
-//            PrintWriter writer = new PrintWriter(outputStream);
-            for(int i = 0; i < this.users.size(); i++) {
-                outputStream.write(this.users.get(i).getFirstName().getBytes());
-                outputStream.write(this.users.get(i).getFirstName().getBytes());
-                outputStream.write((int) this.users.get(i).getBirthDate().getTime());
-                outputStream.write((this.users.get(i).isMale() ? "true" : "false").getBytes());
-                outputStream.write(this.users.get(i).getCountry().getDisplayName().getBytes());
+            PrintStream out = new PrintStream(outputStream);
+            if (users.size() == 0) {
+//                out.print("null");
             }
-    
-            outputStream.flush();
-        }
-
-        public void load(InputStream inputStream) throws Exception {
-            // Implement this method
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            String string;
-
-            String[] strings;
-            while ((string = reader.readLine()) != null) {
-                if(string.equals("")) return;
-                strings = string.split("~");
-                User user = new User();
-                user.setFirstName(strings[0]);
-                user.setLastName(strings[1]);
-                user.setBirthDate(new Date(Long.parseLong(strings[2])));
-                user.setMale(Objects.equals(strings[3], "Yes"));
-                switch (User.Country.valueOf(strings[4])) {
-                    case UNITED_STATES:
-                        user.setCountry(User.Country.UNITED_STATES);
-                        break;
-                    case UNITED_KINGDOM:
-                        user.setCountry(User.Country.UNITED_KINGDOM);
-                        break;
-                    default:
-                        user.setCountry(User.Country.OTHER);
-                        break;
+            else {
+                if (users.size() > 0) {
+                    User temp = users.get(0);
+                    String firstName = temp.getFirstName();
+                    String lastName = temp.getLastName();
+                    String birthday = temp.getBirthDate().toString();
+                    String sex = temp.isMale() ? "true" : "false";
+                    String country = temp.getCountry().toString();
+                    out.print(firstName + "," + lastName + "," + birthday + "," + sex + "," + country);
+                    for (int i = 1; i < users.size(); i++) {
+                        temp = users.get(i);
+                        firstName = temp.getFirstName();
+                        lastName = temp.getLastName();
+                        birthday = temp.getBirthDate().toString();
+                        sex = temp.isMale() ? "true" : "false";
+                        country = temp.getCountry().toString();
+                        out.print("\n" + firstName + "," + lastName + "," + birthday + "," + sex + "," + country);
+                    }
                 }
-    
-    
-                this.users.add(user);
             }
-            reader.close();
+            out.close();
+        }
+    
+        public void load(InputStream inputStream) throws Exception {
+            BufferedReader bR = new BufferedReader(new InputStreamReader(inputStream));
+            String in;
+            while ((in = bR.readLine()) != null) {
+                String[] inStrings = in.split(",");
+                String firstName = inStrings[0];
+                String lastName = inStrings[1];
+                String birthday = inStrings[2];
+                String sex = inStrings[3];
+                String country = inStrings[4];
+            
+                User user = new User();
+            
+                //Name
+                user.setFirstName(firstName);
+                user.setLastName(lastName);
+            
+                //Birthday
+                SimpleDateFormat formatter = new SimpleDateFormat("EEE MMM dd HH:mm:ss zzz yyyy");
+                Date parsedDate = formatter.parse(birthday);
+                user.setBirthDate(parsedDate);
+            
+                //Sex
+                if (sex.equals("true")) {
+                    user.setMale(true);
+                }
+                else {
+                    user.setMale(false);
+                }
+            
+                //country
+                user.setCountry(User.Country.valueOf(country));
+    
+                users.add(user);
+    
+            }
+            bR.close();
         }
 
         @Override
