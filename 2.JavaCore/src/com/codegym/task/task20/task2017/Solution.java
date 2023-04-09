b19e55d3c10105e67e7b6f3143b9e91032fb8a1c
mrbrogrammer <seesimbarun@gmail.com>
2023-04-09 20:23:22 +0200

    Find the bugs

---
+++ 2.JavaCore/src/com/codegym/task/task20/task2017/Solution.java
@@ -0,0 +1,34 @@
+package com.codegym.task.task20.task2017;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.Serializable;
+
+/* 
+Deserialization
+
+*/
+
+public class Solution {
+    public A getOriginalObject(ObjectInputStream objectStream) {
+        try {
+            return (A) objectStream.readObject();
+        } catch (Exception e) {
+            System.out.println("Can not read object");
+            return null;
+        }
+    }
+
+    public class A implements Serializable {
+    }
+
+    public class B extends A {
+        public B() {
+            System.out.println("inside B");
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
