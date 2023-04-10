0d793a7d2d71f6bec139ba53a1eb2950bf9a89e3
mrbrogrammer <seesimbarun@gmail.com>
2023-04-10 12:26:18 +0200

    Task has a bug

--- 2.JavaCore/src/com/codegym/task/task20/task2023/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task20/task2023/Solution.java
@@ -1,28 +1,35 @@
 package com.codegym.task.task20.task2023;
 
-/* 
+/**
 Making the right conclusion
-
+ 
+ C class, method2
+ A class, method2
+ A class, method1
+ B class, method1
 */
 
 public class Solution {
     public static void main(String[] s) {
         A a = new C();
         a.method2();
+        
     }
 
     public static class A {
-        public void method1() {
+        private void method1() {
             System.out.println("A class, method1");
         }
 
         public void method2() {
+            method1();
             System.out.println("A class, method2");
         }
     }
 
     public static class B extends A {
         public void method1() {
+            super.method2();
             System.out.println("B class, method1");
         }
 
