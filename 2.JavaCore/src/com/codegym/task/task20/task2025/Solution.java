fc71be1febe4e7f34fe51669e018d265ba07dc3f
mrbrogrammer <seesimbarun@gmail.com>
2023-04-10 18:06:05 +0200

    Number algorithms solved

--- 2.JavaCore/src/com/codegym/task/task20/task2025/Solution.java
+++ 2.JavaCore/src/com/codegym/task/task20/task2025/Solution.java
@@ -3,6 +3,8 @@ package com.codegym.task.task20.task2025;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 /* 
 Number algorithms
@@ -10,113 +12,134 @@ Number algorithms
 */
 
 public class Solution {
-
-    public static long[] getNumbers(long N) {
-//        ArrayList<Long> chosen = new ArrayList<>();
-//
-//        for (long count = 0; count < N; count++) {
-//            String s = Long.toString(count);
-//            char[] digits = s.toCharArray();
-//
-//            long total =0;
-//
-//            for (char entry : digits) {
-//                if (total > count) break;
-//                int temp = Character.getNumericValue(entry);
-//                if (temp != 0) {
-//                    total += Math.pow(temp, digits.length);
-//                }
-//            }
-//
-//            if (total == count) {
-//                chosen.add(count);
-//            }
-//        }
-//
-//        long[] result = new long[chosen.size()];
-//
-//        for (int i = 0; i < chosen.size(); i++) {
-//            result[i] = chosen.get(i);
-//        }
+    private static int N;
+    private static int[] digitsMultiSet;
+    private static int[] testMultiSet;
+    private static List<Long> results;
+    private static long maxPow;
+    private static long minPow;
+    private static long[][] pows;
     
-    
-        long[] out = new long[]{
-                0,
-                1,
-                2,
-                3,
-                4,
-                5,
-                6,
-                7,
-                8,
-                9,
-                153,
-                370,
-                371,
-                407,
-                1634,
-                8208,
-                9474,
-                54748,
-                92727,
-                93084,
-                548834,
-                1741725,
-                4210818,
-                9800817,
-                9926315,
-                24678050,
-                24678051,
-                88593477,
-                146511208,
-                472335975,
-                534494836,
-                912985153,
-                4679307774L,
-                32164049650L,
-                32164049651L,
-                40028394225L,
-                42678290603L,
-                44708635679L,
-                49388550606L,
-                82693916578L,
-                94204591914L,
-                28116440335967L,
-                4338281769391370L,
-                4338281769391371L,
-                21897142587612075L,
-                35641594208964132L,
-                35875699062250035L,
-                1517841543307505039L,
-                3289582984443187032L,
-                4498128791164624869L,
-                4929273885928088826L
-        };
-    
-        int index = 0;
-        for (int i = 0; i < out.length; i++) {
-            if (out[i] > N) {
-                
-                index = i;
-                break;
+    private static void genPows(int N) {
+//        if (N > 20) throw new IllegalArgumentException();
+        pows = new long[10][N + 1];
+        for (int i = 0; i < pows.length; i++) {
+            long p = 1;
+            for (int j = 0; j < pows[i].length; j++) {
+                pows[i][j] = p;
+                p *= i;
             }
         }
+    }
     
-        return Arrays.copyOf(out, index);
+    private static boolean check(long pow) {
+        if (pow >= maxPow) return false;
+        if (pow < minPow) return false;
+        
+        for (int i = 0; i < 10; i++) {
+            testMultiSet[i] = 0;
+        }
+        
+        while (pow > 0) {
+            int i = (int) (pow % 10);
+            testMultiSet[i]++;
+            if (testMultiSet[i] > digitsMultiSet[i]) return false;
+            pow = pow / 10;
+        }
+        
+        for (int i = 0; i < 10; i++) {
+            if (testMultiSet[i] != digitsMultiSet[i]) return false;
+        }
+        
+        return true;
+    }
+    
+    private static void search(int digit, int unused, long pow) {
+        if (pow >= maxPow) return;
+        
+        if (digit == -1) {
+            if (check(pow)) results.add(pow);
+            return;
+        }
+        
+        if (digit == 0) {
+            digitsMultiSet[digit] = unused;
+            search(digit - 1, 0, pow + unused * pows[digit][N]);
+        } else {
+            // Check if we can generate more than minimum
+            if (pow + unused * pows[digit][N] < minPow) return;
+            
+            long p = pow;
+            for (int i = 0; i <= unused; i++) {
+                digitsMultiSet[digit] = i;
+                search(digit - 1, unused - i, p);
+                if (i != unused) {
+                    p += pows[digit][N];
+                    // Check maximum and break the loop - doesn't help
+                    // if (p >= maxPow) break;
+                }
+            }
+        }
+    }
+    
+    public static List<Long> generate(int maxN) {
+//        if (maxN > 20) throw new IllegalArgumentException();
+        
+        genPows(maxN);
+        results = new ArrayList<>();
+        digitsMultiSet = new int[10];
+        testMultiSet = new int[10];
+        
+        for (N = 1; N <= maxN; N++) {
+            minPow = (long) Math.pow(10, N - 1);
+            maxPow = (long) Math.pow(10, N);
+            
+            search(9, N, 0);
+        }
+        
+        Collections.sort(results);
+        
+        return results;
+    }
+
+    public static long[] getNumbers(long N) {
+        
+        int len = String.valueOf(N).length();
+    
+    
+        List<Long> g = generate(len);
+        long[] result = new long[g.size()];
+        int index = 0;
+        for (; index < g.size(); index++) {
+            if (g.get(index) >= N) {
+                break;
+            }
+            result[index] = g.get(index);
+        }
+        
+        result = Arrays.copyOf(result, index);
+        return result;
     }
 
     public static void main(String[] args) throws IOException {
-        long a = System.currentTimeMillis();
-        System.out.println(Arrays.toString(getNumbers(1000000L)));
-        long b = System.currentTimeMillis();
-        System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 1024));
-        System.out.println("time = " + (b - a) / 1000);
-
-        a = System.currentTimeMillis();
-        System.out.println(Arrays.toString(getNumbers(1000L)));
-        b = System.currentTimeMillis();
-        System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 1024));
-        System.out.println("time = " + (b - a) / 1000);
+        long[] ns = new long[]{8208L, 370L};
+    
+        for (int i = 0; i < ns.length; i++) {
+            long N = ns[i];
+            System.out.println("Result for N = " + N);
+            System.out.println(Arrays.toString(Solution.getNumbers(N)));
+        }
+        
+//        long a = System.currentTimeMillis();
+//        System.out.println(Arrays.toString(getNumbers(1000000L)));
+//        long b = System.currentTimeMillis();
+//        System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 1024));
+//        System.out.println("time = " + (b - a) / 1000);
+//
+//        a = System.currentTimeMillis();
+//        System.out.println(Arrays.toString(getNumbers(1000L)));
+//        b = System.currentTimeMillis();
+//        System.out.println("memory " + (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / (8 * 1024));
+//        System.out.println("time = " + (b - a) / 1000);
     }
 }
