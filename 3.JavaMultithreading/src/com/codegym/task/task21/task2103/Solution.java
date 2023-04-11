07b3c7f75dba193f715e807775f9282d193e8ab2
mrbrogrammer <seesimbarun@gmail.com>
2023-04-11 20:35:53 +0200

    Everything ingenious is simple! solved

---
+++ 3.JavaMultithreading/src/com/codegym/task/task21/task2103/Solution.java
@@ -0,0 +1,16 @@
+package com.codegym.task.task21.task2103;
+
+/* 
+Everything ingenious is simple!
+
+*/
+
+public class Solution {
+    public static boolean calculate(boolean a, boolean b, boolean c, boolean d) {
+        return c; // Only one argument of the method is important
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
