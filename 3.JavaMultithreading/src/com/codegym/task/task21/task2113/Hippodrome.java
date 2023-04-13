c608a2f89fe6c7a990bb3efa11b8269b2b63b42a
mrbrogrammer <seesimbarun@gmail.com>
2023-04-13 17:28:32 +0200

    Hippodrome part 6 solved

--- 3.JavaMultithreading/src/com/codegym/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/codegym/task/task21/task2113/Hippodrome.java
@@ -5,8 +5,17 @@ import java.util.List;
 
 public class Hippodrome {
     public static void main(String[] args) {
+        
+        game = new Hippodrome(new ArrayList<>());
+        game.getHorses().add(new Horse("Stallion", 3, 0));
+        game.getHorses().add(new Horse("Racer", 3, 0));
+        game.getHorses().add(new Horse("Ivy", 3, 0));
+        
+        
     }
     private List<Horse> horses;
+    
+    static Hippodrome game;
     
     public List<Horse> getHorses() {
         return horses;
