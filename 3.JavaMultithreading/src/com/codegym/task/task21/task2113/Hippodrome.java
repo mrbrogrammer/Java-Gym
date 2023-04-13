4268bcb23562cd505c1cad8030e061336299a118
mrbrogrammer <seesimbarun@gmail.com>
2023-04-13 14:18:46 +0200

    Hippodrome part 2 solved

--- 3.JavaMultithreading/src/com/codegym/task/task21/task2113/Hippodrome.java
+++ 3.JavaMultithreading/src/com/codegym/task/task21/task2113/Hippodrome.java
@@ -1,7 +1,14 @@
 package com.codegym.task.task21.task2113;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Hippodrome {
-    
     public static void main(String[] args) {
+    }
+    private List<Horse> horses = new ArrayList<>();
+    
+    public List<Horse> getHorses() {
+        return horses;
     }
 }
